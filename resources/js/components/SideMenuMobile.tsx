e0685fcdf33eab228e53ace38fe549cd9c0622a1
brunoberte <brunoberte@gmail.com>
2025-12-22 12:02:55 -0300

    update mobile menu

--- resources/js/components/SideMenuMobile.tsx
+++ resources/js/components/SideMenuMobile.tsx
@@ -8,7 +8,8 @@ import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
 import MenuButton from './MenuButton';
 import MenuContent from './MenuContent';
-import CardAlert from './CardAlert';
+import { usePage } from '@inertiajs/react';
+import type { SharedData } from '@/types';
 
 interface SideMenuMobileProps {
   open: boolean | undefined;
@@ -16,56 +17,48 @@ interface SideMenuMobileProps {
 }
 
 export default function SideMenuMobile({ open, toggleDrawer }: SideMenuMobileProps) {
+  const { auth } = usePage<SharedData>().props;
   return (
-    <Drawer
-      anchor="right"
-      open={open}
-      onClose={toggleDrawer(false)}
-      sx={{
-        zIndex: (theme) => theme.zIndex.drawer + 1,
-        [`& .${drawerClasses.paper}`]: {
-          backgroundImage: 'none',
-          backgroundColor: 'background.paper',
-        },
-      }}
-    >
-      <Stack
-        sx={{
-          maxWidth: '70dvw',
-          height: '100%',
-        }}
+      <Drawer
+          anchor="right"
+          open={open}
+          onClose={toggleDrawer(false)}
+          sx={{
+              zIndex: (theme) => theme.zIndex.drawer + 1,
+              [`& .${drawerClasses.paper}`]: {
+                  backgroundImage: 'none',
+                  backgroundColor: 'background.paper',
+              },
+          }}
       >
-        <Stack direction="row" sx={{ p: 2, pb: 0, gap: 1 }}>
           <Stack
-            direction="row"
-            sx={{ gap: 1, alignItems: 'center', flexGrow: 1, p: 1 }}
+              sx={{
+                  maxWidth: '70dvw',
+                  height: '100%',
+              }}
           >
-            <Avatar
-              sizes="small"
-              alt="Riley Carter"
-              src="/static/images/avatar/7.jpg"
-              sx={{ width: 24, height: 24 }}
-            />
-            <Typography component="p" variant="h6">
-              Riley Carter
-            </Typography>
+              <Stack direction="row" sx={{ p: 2, pb: 0, gap: 1 }}>
+                  <Stack direction="row" sx={{ gap: 1, alignItems: 'center', flexGrow: 1, p: 1 }}>
+                      <Avatar sizes="small" alt={auth.user.name} src="/static/images/avatar/7.jpg" sx={{ width: 24, height: 24 }} />
+                      <Typography component="p" variant="h6">
+                          {auth.user.name}
+                      </Typography>
+                  </Stack>
+                  <MenuButton showBadge>
+                      <NotificationsRoundedIcon />
+                  </MenuButton>
+              </Stack>
+              <Divider />
+              <Stack sx={{ flexGrow: 1 }}>
+                  <MenuContent />
+                  <Divider />
+              </Stack>
+              <Stack sx={{ p: 2 }}>
+                  <Button variant="outlined" fullWidth startIcon={<LogoutRoundedIcon />}>
+                      Logout
+                  </Button>
+              </Stack>
           </Stack>
-          <MenuButton showBadge>
-            <NotificationsRoundedIcon />
-          </MenuButton>
-        </Stack>
-        <Divider />
-        <Stack sx={{ flexGrow: 1 }}>
-          <MenuContent />
-          <Divider />
-        </Stack>
-        <CardAlert />
-        <Stack sx={{ p: 2 }}>
-          <Button variant="outlined" fullWidth startIcon={<LogoutRoundedIcon />}>
-            Logout
-          </Button>
-        </Stack>
-      </Stack>
-    </Drawer>
+      </Drawer>
   );
 }
